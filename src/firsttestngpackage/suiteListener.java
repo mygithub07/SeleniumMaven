19ea1a273dde5b8cbeb0e8fee4f027d974bb5164
Amit <mygithub07@users.noreply.github.com>
2019-10-15 12:09:50 -0400

    Update suiteListener.java

--- src/firsttestngpackage/suiteListener.java
+++ src/firsttestngpackage/suiteListener.java
@@ -58,11 +58,11 @@ public class suiteListener implements ISuiteListener {
 	    
 	     for (ITestResult tempP : passedTests) {
             ITestNGMethod methodP = tempP.getMethod();
-              System.out.println("passed method...."+ context.getPassedTests().getResults(method).getName());
+              System.out.println("passed method...."+ context.getPassedTests().getResults(methodP).getName());
                 }
               for (ITestResult tempF : failedTests) {
             ITestNGMethod methodF = tempF.getMethod();
-              System.out.println("passed method...."+ context.failedTests().getResults(method).getName());
+              System.out.println("passed method...."+ context.failedTests().getResults(methodF).getName());
                 }
 		     
 	/*     
