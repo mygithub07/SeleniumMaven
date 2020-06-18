30965560b5c80879dbdd0ef04ec7dce8dd6d1dea
Amit <mygithub07@users.noreply.github.com>
2020-06-18 12:21:44 -0400

    Update LoginPageTest.java

--- src/firsttestngpackage/LoginPageTest.java
+++ src/firsttestngpackage/LoginPageTest.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import java.util.ArrayList;
 import java.util.List;
 import org.testng.annotations.Guice;
-import com.google.inject.Inject;
+import com.google.inject;
 
 
 @Guice(modules = {PageObjectOne.class})
