da66f60f185503b742963208f8fad7aa0ad3a35d
Amit <mygithub07@users.noreply.github.com>
2020-06-19 13:40:22 -0400

    Update DriverInitiate.java

--- src/firsttestngpackage/DriverInitiate.java
+++ src/firsttestngpackage/DriverInitiate.java
@@ -7,25 +7,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import org.testng.annotations.Guice;
+import com.google.inject.Inject;
 
-public class DriverInitiate {
+public class DriverInitiate implements Module{
+	
 	WebDriver WebDriver;
 	String baseUrl, nodeURL, url;
 
-   private static DriverInitiate driverInstance = new DriverInitiate( );
+  // private static DriverInitiate driverInstance = new DriverInitiate( );
 
    /* A private Constructor prevents any other
     * class from instantiating.
     */
-   private DriverInitiate() { }
+ //  private DriverInitiate() { }
 
    /* Static 'instance' method */
+	/*
    public static DriverInitiate getInstance( ) {
       return driverInstance;
    }
-
+*/
+	
    /* Other methods protected by singleton-ness */
-   public  WebDriver InitiateDriver() throws MalformedURLException {
+/*	
+   public  WebDriver InitiateDriver() throws MalformedURLException { */
+	
 	   /*  baseUrl= "http://newtours.demoaut.com/" ;
 	      nodeURL= "http://localhost:4444/wd/hub" ;
 	      
@@ -37,6 +46,7 @@ public class DriverInitiate {
 	    //  WebDriver = new RemoteWebDriver (new URL(nodeURL), capability);
 	     WebDriver = new RemoteWebDriver (capability);
 	     */
+	   /*
 	   url= "http://hp.com";
 	   DesiredCapabilities desiredCapabilities =  DesiredCapabilities.chrome();
 	   final ChromeOptions chromeOptions = new ChromeOptions();
@@ -47,5 +57,25 @@ public class DriverInitiate {
 	   //WebDriver driver = new RemoteWebDriver(desiredCapabilities);
 	      return driver;
 	      
-	  }
+	  } */
+	
+	
+	 public  void configure() throws MalformedURLException { 
+	        DesiredCapabilities capability =  DesiredCapabilities.chrome();
+	     
+	      capability.setBrowserName("chrome");
+	      capability.setPlatform(Platform.LINUX);
+	      capability.setVersion("");
+		 url= "http://hp.com";
+	   DesiredCapabilities desiredCapabilities =  DesiredCapabilities.chrome();
+	   final ChromeOptions chromeOptions = new ChromeOptions();
+           chromeOptions.setBinary("/usr/bin/google-chrome-stable");
+           chromeOptions.addArguments("--headless");
+           desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+           WebDriver driver = new RemoteWebDriver(new URL(url), desiredCapabilities);
+		  binder.bind(WebDriver.class).toInstance(driver);
+	 }
+	
+	
+	
 }
