1cf7bcc24c17cca93b8053e759245da5ebb4770a
Amit <mygithub07@users.noreply.github.com>
2020-06-25 15:50:11 -0400

    Update Main.java

--- src/firsttestngpackage/Main.java
+++ src/firsttestngpackage/Main.java
@@ -2,8 +2,8 @@ import firsttestngpackage.Inj;
 
 public class Main {
   
-     public  static injector inject;
+     
      public static void main(String[] args) {}
       
-          inject = new injector();
+         injector inject = new injector();
    }
