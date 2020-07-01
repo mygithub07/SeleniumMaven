6c0757067b1895a3a4cb145c05945e134301d753
Amit <mygithub07@users.noreply.github.com>
2020-07-01 14:33:28 -0400

    Update FirstTestNGFile.java

--- src/firsttestngpackage/FirstTestNGFile.java
+++ src/firsttestngpackage/FirstTestNGFile.java
@@ -17,18 +17,18 @@ import com.google.inject.Injector;
 import firsttestngpackage.Inj;
 
 //import firsttestngpackage.CustomReport;
-@Guice(modules = {DriverInitiate.class})
+//@Guice(modules = {DriverInitiate.class})
 
 public class FirstTestNGFile {	
   
-  @Inject  WebDriver driver;
-// public static Injector inj = Inj.injector;
-// public useDriver usedriver = inj.getInstance(useDriver.class);	
+ // @Inject  WebDriver driver;
+ public static Injector inj = Inj.injector;
+ public useDriver usedriver = inj.getInstance(useDriver.class);	
   
    @Test(groups={"smoke"})
 	public void FirstTestNGFileTest(){
-	    System.out.println("driver from FirstTestNGFileTest method " + driver);
-      // System.out.println("driver from FirstTestNGFileTest method " + usedriver.getTheDriver());
+    //    System.out.println("driver from FirstTestNGFileTest method " + driver);
+       System.out.println("driver from FirstTestNGFileTest method " + usedriver.getTheDriver());
 	} 
   /*
   @Test(groups={"sanity"}) 
