f7d44aa4854e5dd1beceaaa13e35b2ccce55cdee
Amit <mygithub07@users.noreply.github.com>
2020-07-01 13:54:17 -0400

    Update DriverInitiate.java

--- src/firsttestngpackage/DriverInitiate.java
+++ src/firsttestngpackage/DriverInitiate.java
@@ -1,4 +1,4 @@
-
+/*
 package firsttestngpackage;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -25,7 +25,8 @@ public class DriverInitiate implements Module{
 	
 
    private static DriverInitiate driverInstance = new DriverInitiate( );
-
+*/
+	
    /* A private Constructor prevents any other
     * class from instantiating.
     */
@@ -67,7 +68,7 @@ public class DriverInitiate implements Module{
 	  } 
 	*/
 	
-
+/*
 	public  static WebDriver InitiateDriver() throws MalformedURLException { 
 	        DesiredCapabilities capability =  DesiredCapabilities.chrome();
 	     
@@ -102,6 +103,6 @@ public class DriverInitiate implements Module{
 		}
 	}
 	
-}
+} */
 
 
